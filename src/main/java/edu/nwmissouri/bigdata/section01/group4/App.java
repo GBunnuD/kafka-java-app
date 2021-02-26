47d143cae645cc8d0b827750c3715ad42569215a
RaviTeja444 <pagidojuraviteja2@gmail.com>
2021-02-25 20:33:54 -0600

    Added Even Number Fraud detection code

--- src/main/java/edu/nwmissouri/bigdata/section01/group4/App.java
+++ src/main/java/edu/nwmissouri/bigdata/section01/group4/App.java
@@ -6,8 +6,10 @@ package edu.nwmissouri.bigdata.section01.group4;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        //Even number fraud detection
+        EvenNumber evenNumber=new EvenNumber();
+        evenNumber.FraudDetectEvenNumber();
     }
 }
